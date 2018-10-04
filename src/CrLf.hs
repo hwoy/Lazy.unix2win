7e41711e687baa03df235ef9d2db68780e51d101
Watthanachai Dueanklang <bosskillerz@gmail.com>
2018-10-04 13:58:41 +0700

    modified:   src/CrLf.hs

--- src/CrLf.hs
+++ src/CrLf.hs
@@ -4,10 +4,10 @@ import Data.Word
 data CRLF a = WIN | MAC a | UNIX a | OTHER a a deriving(Show)
 
 cr::Word8
-cr = 13
+cr = 10
 
 lf::Word8
-lf = 10
+lf = 13
 
 parsing a b
              | a==cr && b==lf = WIN
